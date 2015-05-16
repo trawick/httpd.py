fe8aa59d8ae833775609502c1f373bca87ef455c
Jeff Trawick <trawick@gmail.com>
2015-05-16 19:52:21 -0400

    minor improvements

--- uWSGI-websocket/run_via_uwsgi.sh
+++ uWSGI-websocket/run_via_uwsgi.sh
@@ -2,8 +2,8 @@
 VENV=/home/trawick/envs/httpd.py
 # gevent parameter needed to support more than one websocket
 # thread (i.e., set up gevent)
-${VENV}/bin/uwsgi --http-socket :3007 \
+${VENV}/bin/uwsgi --http-socket 127.0.0.1:3007 \
   --http-raw-body \
-  --gevent 4 \
+  --gevent 100 \
   --wsgi-file app.py \
   --chdir /home/trawick/git/httpd.py/uWSGI-websocket
