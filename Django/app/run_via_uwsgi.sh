71f0aba50fc67b08200dfe3f6d13b7c5a9358c2e
Jeff Trawick <trawick@gmail.com>
2015-05-18 17:21:14 -0400

    Put uWSGI args on separate lines, axe extra arg for Django app

--- Django/app/run_via_uwsgi.sh
+++ Django/app/run_via_uwsgi.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 VENV=/home/trawick/envs/httpd.py
-${VENV}/bin/uwsgi --scgi-socket 127.0.0.1:3006 --wsgi-file app.py --module app.wsgi --chdir /home/trawick/git/httpd.py/Django/app --virtualenv ${VENV}
+${VENV}/bin/uwsgi --scgi-socket 127.0.0.1:3006 \
+  --module app.wsgi \
+  --chdir /home/trawick/git/httpd.py/Django/app \
+  --virtualenv ${VENV}
